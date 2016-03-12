0c82d9ba4c4f0fa1d01b6a45cca2de97215f827b
IM Hoang Thai <hoangthai95ne@gmail.com>
2016-03-12 09:12:20 +0700

    Initial Commit

---
+++ UITableViewBasic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewBasic
+//
+//  Created by Hoàng Thái on 3/12/16.
+//  Copyright © 2016 techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewBasicTests/UITableViewBasicTests.m
@@ -0,0 +1,39 @@
+//
+//  UITableViewBasicTests.m
+//  UITableViewBasicTests
+//
+//  Created by Hoàng Thái on 3/12/16.
+//  Copyright © 2016 techmaster. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UITableViewBasicTests : XCTestCase
+
+@end
+
+@implementation UITableViewBasicTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
